c9c8103aac767519448fd40b8911ed9150880bc0
Vipul Chauhan <viratdev07@gmail.com>
2020-12-20 10:25:51 +0530

    Back to originals

--- src/main/java/com/example/demojpa4/demo/Model/Book.java
+++ src/main/java/com/example/demojpa4/demo/Model/Book.java
@@ -3,12 +3,10 @@ package com.example.demojpa4.demo.Model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "my_book")
 public class Book {
 
     @Id
     private int id;
-    @Column(name = "book_name")
     private String name;
     private String authorName;
     private int cost;
@@ -17,32 +15,16 @@ public class Book {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getAuthorName() {
         return authorName;
     }
 
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
     public int getCost() {
         return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
     }
 
     public Book(int id, String name, String authorName, int cost) {
